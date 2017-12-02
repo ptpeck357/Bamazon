df56b32fea04c86317bd9581b4bbef6881ce0cfa
Peter Peck <karatekid4christ@gmail.com>
2017-12-01 19:39:38 -0600

    Polishing files

---
+++ bamazon.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE database bamazon;
+
+USE bamazon;
+
+Drop TABLE IF EXISTS products;
+
+CREATE TABLE products (
+  item_id INT NOT NULL,
+  product_name VARCHAR(100) NULL,
+  department_name VARCHAR(100) NULL,
+  price INT NULL,
+  stock_quantity INT NULL,
+  product_sales INT NULL,
+  PRIMARY KEY(item_id)
+  );
+  
+SELECT * FROM products;
+
+
+
